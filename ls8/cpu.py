5206db91f8e041a2f45b4a4c760664812b6c420c
Agustina Sosa <agustinasf85@gmail.com>
2019-11-12 16:52:27 -0500

    MVP day 1 complete + started workin on load()

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -23,22 +23,33 @@ class CPU:
         """Load a program into memory."""
 
         address = 0
+        if len(sys.argv) < 2:
+            print('The program name is required')
+            sys.exit()
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+        while open(sys.argv[1]) as f:
+            for line in f:
+                if line[0] != '#' and line != '\n':
+                    self.ram[address] = int(line[0:8], 2)
+                    address += 1
+                    
+                f.closed
+
 
 
     def alu(self, op, reg_a, reg_b):
@@ -47,6 +58,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -72,4 +85,30 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN  = 0b01000111 
+        PUSH = 0b01000101 
+        POP  = 0b01000110 
+        MUL  = 0b10100010
+        running = True
+
+        while running:
+            IR = self.ram[self.pc]
+
+            operand_a = self.ram_read(self.pc +1)
+            operand_b = self.ram_read(self.pc +2)
+            if IR == HLT:
+                running = False
+            elif IR == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif IR == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+            elif IR == MUL:
+                self.alu("MUL ->", operand_a, operand_b)
+                self.pc += 3
+            else:
+                print("HALTED")
+                running = False
