138645ec757e599b94407b06fc072755e2559830
Agustina Sosa <agustinasf85@gmail.com>
2019-11-15 11:50:25 -0500

    MVP complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,17 @@
 
 import sys
 
+HLT = 0b00000001
+LDI = 0b10000010
+PRN  = 0b01000111 
+PUSH = 0b01000101 
+POP  = 0b01000110 
+MUL  = 0b10100010
+ADD = 0b10100000
+CALL = 0b01010000
+RET = 0b00010001
+
+
 class CPU:
     """Main CPU class."""
 
@@ -10,6 +21,18 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
+        self.SP = 0x07
+        self.dsptchtbl = {
+            LDI: self.LDI,
+            PRN: self.PRN,
+            PUSH: self.PUSH,
+            POP: self.POP,
+            MUL: self.MUL,
+            ADD: self.ADD,
+            CALL: self.CALL,
+            RET: self.RET
+        }
+
 
     def ram_read(self, MAR): # Memory Access Register
         # Finds and returns value at given address (= index)
@@ -24,33 +47,15 @@ class CPU:
 
         address = 0
         if len(sys.argv) < 2:
-            print('The program name is required')
+            print("The program name is required")
             sys.exit()
-
-        # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
+        
         while open(sys.argv[1]) as f:
             for line in f:
                 if line[0] != '#' and line != '\n':
                     self.ram[address] = int(line[0:8], 2)
                     address += 1
-                    
-                f.closed
-
-
+            f.closed
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -83,14 +88,39 @@ class CPU:
 
         print()
 
+    def LDI(self, operand_a, operand_b):
+        self.reg[operand_a] = operand_b
+        self.pc += 3
+    def PRN(self, operand_a, operand_b):
+        print(self.reg[operand_a])
+        self.pc += 2
+    def MUL(self, operand_a, operand_b):
+        self.alu("MUL", operand_a, operand_b)
+        self.pc += 3
+    def ADD(self, operand_a, operand_b):
+        self.alu("ADD", operand_a, operand_b)
+        self.pc += 3
+    def PUSH(self, operand_a, operand_b):
+        self.SP -= 1
+        self.ram[self.SP] = self.reg[operand_a]
+        self.pc += 2
+    def POP(self, operand_a, operand_b):
+        self.reg[operand_a] = self.ram[self.SP]
+        self.SP += 1
+        self.pc += 2
+    def CALL(self, operand_a, operand_b):
+        ret_add = self.pc + 2
+        self.reg[self.SP] -= 1
+        self.ram[self.reg[self.SP]] = ret_add
+        self.pc = self.reg[operand_a]
+    def RET(self, operand_a, operand_b):
+        ret_add = self.ram[self.reg[self.SP]]
+        self.reg[self.SP] += 1
+        self.pc = ret_add
+
+
     def run(self):
         """Run the CPU."""
-        HLT = 0b00000001
-        LDI = 0b10000010
-        PRN  = 0b01000111 
-        PUSH = 0b01000101 
-        POP  = 0b01000110 
-        MUL  = 0b10100010
         running = True
 
         while running:
@@ -100,15 +130,5 @@ class CPU:
             operand_b = self.ram_read(self.pc +2)
             if IR == HLT:
                 running = False
-            elif IR == LDI:
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-            elif IR == PRN:
-                print(self.reg[operand_a])
-                self.pc += 2
-            elif IR == MUL:
-                self.alu("MUL ->", operand_a, operand_b)
-                self.pc += 3
             else:
-                print("HALTED")
-                running = False
+                self.dsptchtbl[IR](operand_a, operand_b)
